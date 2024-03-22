ab23ab1ce7962200076dc90b9472f9b5e0640be9
ayushagrawal <ayushagrawal6082@gmail.com>
2024-03-22 19:53:23 +0530

    Done

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+        <p>Copyright@2024 | All Rights Reserved | by Ayush Agrawal</p>
+    </div>
+  )
+}
+
+export default Footer
